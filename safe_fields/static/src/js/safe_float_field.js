94d3f1a76e3c2a785fda0706b1917419eceeaadd
sholah15 <136168180+sholah15@users.noreply.github.com>
2026-06-06 08:14:47 +0700

    perbaikan error javascript

--- safe_fields/static/src/js/safe_float_field.js
+++ safe_fields/static/src/js/safe_float_field.js
@@ -1,17 +1,22 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { FloatField } from "@web/views/fields/float/float_field";
+
 export class SafeFloatField extends FloatField {
 
     onInput(ev) {
-        let value = ev.target.value;
+        let value = ev.target.value || "";
 
-        value = value.replace(/[eE]/g, '');
-        value = value.replace(/[^\d\.\-]/g, '');
+        value = value.replace(/[eE]/g, "");
+        value = value.replace(/[^\d.\-]/g, "");
 
-        const parts = value.split('.');
+        const parts = value.split(".");
         if (parts.length > 2) {
-            value = parts[0] + '.' + parts.slice(1).join('');
+            value = parts[0] + "." + parts.slice(1).join("");
         }
 
-        value = value.replace(/(?!^)-/g, '');
+        value = value.replace(/(?!^)-/g, "");
 
         ev.target.value = value;
 
