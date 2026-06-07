c05cd9df10a5691bd5895094c45c9da199409f82
Maizar Rahman <maizarrahman1@gmail.com>
2026-06-07 10:43:18 +0700

    tambah modul untuk uji coba safe_fields

---
+++ safe_fields_tester/models/__init__.py
@@ -0,0 +1,1 @@
+from . import safe_fields_tester
---
+++ safe_fields_tester/models/safe_fields_tester.py
@@ -0,0 +1,14 @@
+from odoo import models, fields
+
+class SafeFieldsTester(models.Model):
+    _name = "safe.fields.tester"
+    _description = "Safe Fields Tester"
+
+    name = fields.SafeChar("Name")
+    notes = fields.SafeText("Notes")
+    distance = fields.SafeFloat("Distance (KM)")
+    qty = fields.SafeInteger("Quantity")
+    price = fields.SafeMonetary("Price")
+    currency_id = fields.Many2one("res.currency", "Currency")
+    
+
